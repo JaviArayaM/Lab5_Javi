e5b829eced19bd0220b6b63e277fbb226daa31c2
JaviArayaM <48633103+JaviArayaM@users.noreply.github.com>
2021-11-18 18:14:20 -0300

    Add files via upload

---
+++ dbcon.php
@@ -0,0 +1,15 @@
+<?php
+$dbuser = 'root';
+$dbpass = '';
+$dbname = 'bdprueba';
+$con = mysqli_connect("localhost",$dbuser,$dbpass,$dbname);
+
+// Check connection
+if (mysqli_connect_errno())
+  {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  }
+
+  date_default_timezone_set("Asia/Manila"); 
+?>
+
---
+++ log.php
@@ -0,0 +1,24 @@
+<?php session_start();
+?>
+
+<?php 
+include('dist/includes/dbcon.php');
+
+
+$protocol = ((!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off') || $_SERVER['SERVER_PORT'] == 443) ? "https://" : "http://"; 
+$currentURL = $protocol . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'] . $_SERVER['QUERY_STRING']; 
+ 
+
+$user_ip_address = $_SERVER['REMOTE_ADDR']; 
+$referrer_url = !empty($_SERVER['HTTP_REFERER'])?$_SERVER['HTTP_REFERER']:'/'; 
+$user_agent = $_SERVER['HTTP_USER_AGENT']; 
+ 
+
+$sql = "INSERT INTO visitor_logs (page_url, referrer_url, user_ip_address, user_agent, created) VALUES (?,?,?,?,NOW())"; 
+$stmt = $db->prepare($sql); 
+$stmt->bind_param("ssss", $currentURL, $referrer_url, $user_ip_address, $user_agent); 
+$insert = $stmt->execute(); 
+
+?>
+
+	
