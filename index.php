e8f65805624ccd9d974ac24269ccb9b5cfc969f8
JaviArayaM <48633103+JaviArayaM@users.noreply.github.com>
2021-11-18 19:10:44 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,3 +1,5 @@
+<?php session_start();
+?>
 <?php
 $id_usuario=$_SERVER['REMOTE_ADDR'];
 ?>
@@ -165,3 +167,22 @@ $id_usuario=$_SERVER['REMOTE_ADDR'];
 ?>
 </body>
 </html>
+
+<?php
+include('dbcon.php');
+
+
+$protocol = ((!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off') || $_SERVER['SERVER_PORT'] == 443) ? "https://" : "http://";
+$currentURL = $protocol . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'] . $_SERVER['QUERY_STRING'];
+ 
+$user_ip_address = $_SERVER['REMOTE_ADDR'];
+$referrer_url = !empty($_SERVER['HTTP_REFERER'])?$_SERVER['HTTP_REFERER']:'/';
+$user_agent = $_SERVER['HTTP_USER_AGENT'];
+ 
+
+$sql = "INSERT INTO visitor_logs (page_url, referrer_url, user_ip_address, user_agent, created) VALUES (?,?,?,?,NOW())";
+$stmt = $db->prepare($sql);
+$stmt->bind_param("ssss", $currentURL, $referrer_url, $user_ip_address, $user_agent);
+$insert = $stmt->execute();
+
+?>
